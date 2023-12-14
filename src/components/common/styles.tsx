27a22f6c1938a75aa8b5de183e50d05f96201aca
ursrBaek <ursrbaek@gmail.com>
2023-12-14 19:42:50 +0900

    feat: 공통 스타일 컨테이너 컴포넌트 생성 #2

---
+++ src/components/common/styles.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background-color: #fff;
+  position: relative;
+  margin: 0 auto;
+  min-width: 360px;
+  max-width: 420px;
+  height: 100vh;
+  box-shadow: 0 9px 15px -3px rgb(0 0 0 / 30%);
+  font-family: 'hanna';
+  overflow: hidden;
+  display: flex;
+  flex-flow: column nowrap;
+`;
