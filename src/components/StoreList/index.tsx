a87c865a43291a4137c937326ef811263a54fa3b
ursrBaek <ursrbaek@gmail.com>
2023-12-22 23:57:03 +0900

    feat: 가게 리스트 페이지 공통 UI 적용

--- src/components/StoreList/index.tsx
+++ src/components/StoreList/index.tsx
@@ -1,5 +1,16 @@
+import BottomMenu from 'components/common/BottomMenu';
+import Header from 'components/common/Header';
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 export default function StoreList() {
-  return <div>StoreList page</div>;
+  const { category } = useParams();
+
+  return (
+    <>
+      <Header>{category ? category : '찜한 가게'}</Header>
+
+      <BottomMenu />
+    </>
+  );
 }
