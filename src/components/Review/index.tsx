d2db3cb59e0f85dc2078b867892085710510cc9e
ursrBaek <ursrbaek@gmail.com>
2023-12-27 20:02:45 +0900

    feat: 리뷰 작성 페이지 UI 구현 #17

--- src/components/Review/index.tsx
+++ src/components/Review/index.tsx
@@ -1,5 +1,46 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import { ReviewWrapper } from './styles';
 
-export default function Review() {
-  return <div>Review page</div>;
+function Index() {
+  const [star, setStar] = useState('1');
+  const [content, setContent] = useState('');
+
+  const onChange = useCallback((e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setContent(e.target.value);
+  }, []);
+
+  const onClickStar = useCallback((e: React.MouseEvent<HTMLUListElement>) => {
+    if (e.target instanceof HTMLLIElement) {
+      setStar(e.target.id);
+    }
+  }, []);
+
+  return (
+    <ReviewWrapper>
+      <p className="q-text">식사는 어떠셨나요?</p>
+      <ul onClick={onClickStar}>
+        <li id="1" className="full-star"></li>
+        <li id="2" className={star >= '2' ? 'full-star' : 'empty-star'}></li>
+        <li id="3" className={star >= '3' ? 'full-star' : 'empty-star'}></li>
+        <li id="4" className={star >= '4' ? 'full-star' : 'empty-star'}></li>
+        <li id="5" className={star === '5' ? 'full-star' : 'empty-star'}></li>
+      </ul>
+      <h1>리뷰 작성</h1>
+      <textarea
+        placeholder="100자 이내의 리뷰를 작성해주세요."
+        maxLength={100}
+        onChange={onChange}
+        value={content}
+      ></textarea>
+      <p className="number-of-letters">
+        <span className="input-letters">{content.length <= 100 ? content.length : 100}</span> /{' '}
+        <span className="max-letters">100자</span>
+      </p>
+      <div className="wrap-btn">
+        <button disabled>리뷰 등록</button>
+      </div>
+    </ReviewWrapper>
+  );
 }
+
+export default Index;
---
+++ src/components/Review/styles.tsx
@@ -0,0 +1,82 @@
+import styled from 'styled-components';
+import fullStar from '../../assets/images/fullStar.png';
+import emptyStar from '../../assets/images/emptyStar.png';
+import reviewBackground from '../../assets/images/reviewBackground.jpg';
+
+export const ReviewWrapper = styled.div`
+  padding: 15% 10% 10%;
+  flex-grow: 1;
+  background-color: #fff;
+  background-image: url(${reviewBackground});
+  background-size: 65%;
+  background-repeat: no-repeat;
+  background-position-x: 0;
+  background-position-y: 105%;
+  h1 {
+    font-size: 20px;
+    font-family: 'jua';
+    margin: 30px auto 10px auto;
+  }
+  .q-text {
+    font-family: 'hanna';
+    font-size: 35px;
+    margin: 10px auto;
+    text-align: center;
+  }
+  ul {
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: space-between;
+    margin: 20px auto;
+    width: 100%;
+    li {
+      width: 50px;
+      height: 50px;
+      background-size: cover;
+      cursor: pointer;
+    }
+    .empty-star {
+      background-image: url(${emptyStar});
+    }
+    .full-star {
+      background-image: url(${fullStar});
+    }
+  }
+
+  textarea {
+    font-size: 18px;
+    height: 170px;
+    display: block;
+    width: 100%;
+    resize: none;
+    padding: 5px;
+  }
+  .number-of-letters {
+    font-size: 17px;
+    margin: 10px auto;
+    text-align: right;
+  }
+  .wrap-btn {
+    margin: 30px auto;
+    button {
+      font-family: 'hanna';
+      width: 150px;
+      height: 40px;
+      float: right;
+      border: 0;
+      border-radius: 3px;
+      background-color: #d15c03;
+      color: #fff;
+      font-size: 20px;
+
+      &:hover {
+        background-color: #e99400;
+      }
+
+      &[disabled] {
+        background-color: #9a9a9a;
+        cursor: auto;
+      }
+    }
+  }
+`;
