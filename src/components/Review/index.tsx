ad29dae43112911371a778e1ca39d97399c2762e
ursrBaek <ursrbaek@gmail.com>
2024-08-12 00:58:40 +0900

    feat: 리뷰 작성 및 별점 반영 #61
    - 작성한 별점 및 리뷰 내용 서버 반영
    - 주문리스트의 해당 주문 리뷰 작성 완료 처리
    - 매장별 평균별점 및 리뷰조회

--- src/components/Review/index.tsx
+++ src/components/Review/index.tsx
@@ -1,10 +1,24 @@
 import React, { useCallback, useState } from 'react';
 import { ReviewWrapper } from './styles';
 import PrevButton from 'components/common/PrevButton';
+import { useOrderInfo, useOrderListAction, useUserId, useUserNickname } from 'store';
+import { serverTimestamp } from 'firebase/database';
+import useReviewMutation, { IReviewInfo } from './hooks/useReviewMutation';
+import { useNavigate, useParams } from 'react-router-dom';
 
 function Index() {
+  const navigate = useNavigate();
+
+  const { orderId } = useParams();
+  const userId = useUserId();
+  const { storeId } = useOrderInfo();
+  const nickname = useUserNickname();
+  const { setOrderInit } = useOrderListAction();
+
   const [star, setStar] = useState('1');
   const [content, setContent] = useState('');
+
+  const { mutateAsync, isPending, isError, error } = useReviewMutation();
 
   const onChange = useCallback((e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setContent(e.target.value);
@@ -16,6 +30,30 @@ function Index() {
     }
   }, []);
 
+  const onClickReviewWriteBtn = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    const reviewInfo: IReviewInfo = {
+      storeId,
+      userId,
+      key: orderId as string,
+      review: {
+        nickname,
+        star: +star,
+        content,
+        date: serverTimestamp(),
+      },
+    };
+
+    await mutateAsync(reviewInfo);
+
+    alert('리뷰 등록이 완료되었습니다.');
+    setOrderInit();
+    navigate(`/store/detail/${storeId}`, { replace: true });
+  };
+
+  if (isError) {
+    alert('리뷰 등록 중 오류가 발생했습니다.');
+    console.log(error.message);
+  }
   return (
     <ReviewWrapper>
       <p className="q-text">식사는 어떠셨나요?</p>
@@ -27,18 +65,14 @@ function Index() {
         <li id="5" className={star === '5' ? 'full-star' : 'empty-star'}></li>
       </ul>
       <h1>리뷰 작성</h1>
-      <textarea
-        placeholder="100자 이내의 리뷰를 작성해주세요."
-        maxLength={100}
-        onChange={onChange}
-        value={content}
-      ></textarea>
+      <textarea placeholder="100자 이내의 리뷰를 작성해주세요." maxLength={100} onChange={onChange} value={content}></textarea>
       <p className="number-of-letters">
-        <span className="input-letters">{content.length <= 100 ? content.length : 100}</span> /{' '}
-        <span className="max-letters">100자</span>
+        <span className="input-letters">{content.length <= 100 ? content.length : 100}</span> / <span className="max-letters">100자</span>
       </p>
       <div className="wrap-btn">
-        <button disabled>리뷰 등록</button>
+        <button disabled={isPending} onClick={onClickReviewWriteBtn}>
+          리뷰 등록
+        </button>
       </div>
       <PrevButton isAbsolutePosition={true} />
     </ReviewWrapper>
